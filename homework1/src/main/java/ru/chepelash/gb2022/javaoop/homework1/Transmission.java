905a09d325d2de9ba4f3bda37cd5896281d96d72
Anton Chepelev <chepelash@gmail.com>
2022-09-24 11:19:01 +0300

    Added classes from a meeting

---
+++ homework1/src/main/java/ru/chepelash/gb2022/javaoop/homework1/Transmission.java
@@ -0,0 +1,15 @@
+package ru.chepelash.gb2022.javaoop.homework1;
+
+public class Transmission {
+    private int grNumber;
+
+    public void switchGear (int number) {
+        this.grNumber = number;
+        System.out.printf("Переключение скорости на %d%n", number);
+    }
+
+    public int getGrNumber() {
+        return grNumber;
+    }
+
+}
---
+++ homework1/src/main/java/ru/chepelash/gb2022/javaoop/homework1/Wheel.java
@@ -0,0 +1,7 @@
+package ru.chepelash.gb2022.javaoop.homework1;
+
+public class Wheel {
+    public void rotate() {
+        System.out.println("Колесо крутится");
+    }
+}
