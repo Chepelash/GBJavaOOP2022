b7806ba3cbf9a38305020dbe6e0daa0d6277c46e
Anton Chepelev <chepelash@gmail.com>
2022-09-24 22:00:07 +0300

    added basic methods

--- homework1/src/main/java/ru/chepelash/gb2022/javaoop/homework1/componentTypes/Headlight.java
+++ homework1/src/main/java/ru/chepelash/gb2022/javaoop/homework1/componentTypes/Headlight.java
@@ -1,4 +1,25 @@
 package ru.chepelash.gb2022.javaoop.homework1.componentTypes;
 
 public class Headlight {
+    private final String color;
+    private boolean isOn;
+    public Headlight(String color){
+        this.color = color;
+        isOn = false;
+    }
+    public void turnOn(){
+        if(!isOn)
+            isOn = true;
+    }
+    public void turnOff(){
+        if(isOn)
+            isOn = false;
+    }
+    public boolean isOn(){
+        return isOn;
+    }
+
+    public String getColor() {
+        return color;
+    }
 }
--- homework1/src/main/java/ru/chepelash/gb2022/javaoop/homework1/componentTypes/SteeringWheel.java
+++ homework1/src/main/java/ru/chepelash/gb2022/javaoop/homework1/componentTypes/SteeringWheel.java
@@ -1,4 +1,10 @@
 package ru.chepelash.gb2022.javaoop.homework1.componentTypes;
 
 public class SteeringWheel {
+    public void turnLeft(){
+        System.out.println("Turning left...");
+    }
+    public void turnRight(){
+        System.out.println("Turning right...");
+    }
 }
