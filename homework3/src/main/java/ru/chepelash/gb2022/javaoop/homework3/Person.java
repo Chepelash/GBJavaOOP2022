215d076657381874a84af9e6fc26c48ce9bad09b
Anton Chepelev <chepelash@gmail.com>
2022-10-11 13:05:15 +0300

    Added getters, compareTo and equals methods

--- homework3/src/main/java/ru/chepelash/gb2022/javaoop/homework3/Person.java
+++ homework3/src/main/java/ru/chepelash/gb2022/javaoop/homework3/Person.java
@@ -1,10 +1,41 @@
 package ru.chepelash.gb2022.javaoop.homework3;
 
+import java.util.Objects;
+
 public class Person implements Comparable<Person>{
     private String name;
     private int age;
     private double weight;
     private double height;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Double.compare(person.weight, weight) == 0 && Double.compare(person.height, height) == 0 && name.equals(person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, weight, height);
+    }
 
     public Person(String name, int age, double weight, double height) {
         this.name = name;
@@ -14,7 +45,19 @@ public class Person implements Comparable<Person>{
     }
 
     public int compareTo(Person o) {
-        return 0;
+        int compareResult;
+        // by name
+        if((compareResult = name.compareTo(o.getName())) == 0){
+            // by age from young to old
+            if((compareResult = age - o.getAge()) == 0){
+                // by height from tall to short
+                if((compareResult = (int) (o.getHeight() - height)) == 0){
+                    // by weight from skinny to fat
+                    return (int) (weight - o.getWeight());
+                }
+            }
+        }
+        return compareResult;
     }
 
     @Override
