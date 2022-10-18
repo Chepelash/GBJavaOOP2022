a5409ff1797a8d7aa60decabc2720f139103a694
Anton Chepelev <chepelash@gmail.com>
2022-10-18 20:02:26 +0300

    Update FileServerTest.java

--- homework6/src/test/java/ru/chepelash/gb2022/javaoop/homework6/FileServerTest.java
+++ homework6/src/test/java/ru/chepelash/gb2022/javaoop/homework6/FileServerTest.java
@@ -2,20 +2,15 @@ package ru.chepelash.gb2022.javaoop.homework6;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import ru.chepelash.gb2022.javaoop.homework6.interfaces.Manager;
 import ru.chepelash.gb2022.javaoop.homework6.interfaces.Server;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FileServerTest {
-    private Manager manager;
     private Server server;
     @BeforeEach
     void setUp() {
-        manager = new FileManager("src/test/resources/");
-        server = new FileServer(8000, manager);
+        server = new FileServer(8000);
     }
 
     @Test
