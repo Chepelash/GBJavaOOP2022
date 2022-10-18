47f34cde9fa23541e00968bdd6a58d1431c604d7
Anton Chepelev <chepelash@gmail.com>
2022-10-18 15:58:21 +0300

    Created file manager

---
+++ homework6/src/main/java/FileManager.java
@@ -0,0 +1,39 @@
+import ru.chepelash.gb2022.javaoop.homework6.interfaces.Manager;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FileManager implements Manager {
+    private final String baseDir;
+
+    public FileManager(String baseDir) {
+        this.baseDir = baseDir.endsWith("/") ? baseDir : baseDir + "/";
+    }
+
+
+    @Override
+    public String readFile(String fileName) throws IOException {
+        if(!isFileExists(fileName)){
+            throw new NoSuchFileException(fileName);
+        }
+        return Files.readString(Paths.get(baseDir + fileName));
+    }
+
+    @Override
+    public boolean isFileExists(String fileName) throws IOException {
+        Set<String> fileSet = getFileSet();
+        return fileSet.contains(fileName);
+    }
+
+    @Override
+    public Set<String> getFileSet() throws IOException {
+        Stream<Path> stream = Files.list(Paths.get(baseDir));
+        return stream.filter(Files::isDirectory)
+                .map(Path::getFileName)
+                .map(Path::toString)
+                .collect(Collectors.toSet());
+    }
+}
---
+++ homework6/src/main/java/ru/chepelash/gb2022/javaoop/homework6/interfaces/Manager.java
@@ -0,0 +1,11 @@
+package ru.chepelash.gb2022.javaoop.homework6.interfaces;
+
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.util.Set;
+
+public interface Manager {
+    String readFile(String fileName) throws IOException;
+    boolean isFileExists(String fileName) throws IOException;
+    Set<String> getFileSet() throws IOException;
+}
