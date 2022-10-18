f2db6a86e5567d5ae93ab52472f7320a3403e4d6
Anton Chepelev <chepelash@gmail.com>
2022-10-18 16:23:20 +0300

    Added server class

---
+++ homework6/src/main/java/ru/chepelash/gb2022/javaoop/homework6/FileServer.java
@@ -0,0 +1,60 @@
+package ru.chepelash.gb2022.javaoop.homework6;
+
+import ru.chepelash.gb2022.javaoop.homework6.interfaces.Manager;
+import ru.chepelash.gb2022.javaoop.homework6.interfaces.Server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class FileServer implements Server {
+    private final int port;
+    private final Manager manager;
+
+    public FileServer(int port, Manager manager) {
+        this.port = port;
+        this.manager = manager;
+    }
+    public FileServer(Manager manager){
+        this(8000, manager);
+    }
+
+    @Override
+    public void run() throws IOException {
+        try(ServerSocket serverSocket = new ServerSocket(port)) {
+            boolean running = true;
+            while (running) {
+                Socket socket = serverSocket.accept();
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                     PrintWriter writer = new PrintWriter(socket.getOutputStream())) {
+                    while (!reader.ready()) ;
+                    StringBuilder sb = new StringBuilder();
+                    while (reader.ready()) {
+                        sb.append(reader.readLine());
+                    }
+                    String request = sb.toString().split(" ")[1].substring(1);
+                    String response;
+                    if (request.equalsIgnoreCase("quit")) {
+                        response = "Goodbye";
+                        running = false;
+                    } else if (request.equalsIgnoreCase("getFileList")) {
+                        response = manager.getFileSet().toString();
+                    } else if (manager.isFileExists(request)) {
+                        response = manager.readFile(request);
+                    } else {
+                        response = "Unknown request";
+                    }
+                    writer.println("HTTP/1.1 200 OK");
+                    writer.println("Content-Type: text/html; charset=utf-8");
+                    writer.println();
+                    writer.println(response);
+                    writer.flush();
+
+                }
+            }
+        }
+    }
+}
---
+++ homework6/src/main/java/ru/chepelash/gb2022/javaoop/homework6/interfaces/Server.java
@@ -0,0 +1,7 @@
+package ru.chepelash.gb2022.javaoop.homework6.interfaces;
+
+import java.io.IOException;
+
+public interface Server {
+    void run() throws IOException;
+}
