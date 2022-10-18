ac9f0bbfd1d84c732370b66bd22a7c7544400ed3
Anton Chepelev <chepelash@gmail.com>
2022-10-18 16:31:02 +0300

    Added separate method for handling requests

--- homework6/src/main/java/ru/chepelash/gb2022/javaoop/homework6/FileServer.java
+++ homework6/src/main/java/ru/chepelash/gb2022/javaoop/homework6/FileServer.java
@@ -13,6 +13,7 @@ import java.net.Socket;
 public class FileServer implements Server {
     private final int port;
     private final Manager manager;
+    private ServerSocket serverSocket;
 
     public FileServer(int port, Manager manager) {
         this.port = port;
@@ -22,39 +23,41 @@ public class FileServer implements Server {
         this(8000, manager);
     }
 
-    @Override
-    public void run() throws IOException {
-        try(ServerSocket serverSocket = new ServerSocket(port)) {
-            boolean running = true;
-            while (running) {
-                Socket socket = serverSocket.accept();
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                     PrintWriter writer = new PrintWriter(socket.getOutputStream())) {
-                    while (!reader.ready()) ;
-                    StringBuilder sb = new StringBuilder();
-                    while (reader.ready()) {
-                        sb.append(reader.readLine());
-                    }
-                    String request = sb.toString().split(" ")[1].substring(1);
-                    String response;
-                    if (request.equalsIgnoreCase("quit")) {
-                        response = "Goodbye";
-                        running = false;
-                    } else if (request.equalsIgnoreCase("getFileList")) {
-                        response = manager.getFileSet().toString();
-                    } else if (manager.isFileExists(request)) {
-                        response = manager.readFile(request);
-                    } else {
-                        response = "Unknown request";
-                    }
-                    writer.println("HTTP/1.1 200 OK");
-                    writer.println("Content-Type: text/html; charset=utf-8");
-                    writer.println();
-                    writer.println(response);
-                    writer.flush();
-
+    private void handleRequest() throws IOException {
+        boolean running = true;
+        while (running) {
+            Socket socket = serverSocket.accept();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter writer = new PrintWriter(socket.getOutputStream())) {
+                while (!reader.ready()) ;
+                StringBuilder sb = new StringBuilder();
+                while (reader.ready()) {
+                    sb.append(reader.readLine());
                 }
+                String request = sb.toString().split(" ")[1].substring(1);
+                String response;
+                if (request.equalsIgnoreCase("quit")) {
+                    response = "Goodbye";
+                    running = false;
+                } else if (request.equalsIgnoreCase("getFileList")) {
+                    response = manager.getFileSet().toString();
+                } else if (manager.isFileExists(request)) {
+                    response = manager.readFile(request);
+                } else {
+                    response = "Unknown request";
+                }
+                writer.println("HTTP/1.1 200 OK");
+                writer.println("Content-Type: text/html; charset=utf-8");
+                writer.println();
+                writer.println(response);
+                writer.flush();
             }
         }
+    }
+    @Override
+    public void run() throws IOException {
+        serverSocket = new ServerSocket(port);
+        handleRequest();
+        serverSocket.close();
     }
 }
